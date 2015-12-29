be0492fb3da2f45520a9b43c3db71aa2b76daaf0
Eric Mugerwa <ericmdev@gmail.com>
2015-12-29 02:10:53 +0000

    Add usage example

--- example/index.php
+++ example/index.php
@@ -1,3 +1,9 @@
 <?php
 require '../vendor/autoload.php';
 use MyVendorName\MyPackageName\MyClass;
+
+$MyClass = new MyClass(['opt-1' => true]);
+echo 'MYCONST: ', $MyClass::MYCONST, "\n";
+$MyClass->myMethod("Hello, World!");
+$MyClass->myTraitMethod(300);
+$MyClass->myFinalMethod(true);
