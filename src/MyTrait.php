ec6446318a69494534527f7109de0c61b251044c
Eric Mugerwa <ericmdev@gmail.com>
2015-12-29 01:44:30 +0000

    Add src/MyTrait.php

---
+++ src/MyTrait.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * MyVendorName (http://example.io)
+ *
+ * @package   MyPackageName
+ * @link      https://github.com/user/repo
+ * @license   http://opensource.org/licenses/MIT MIT
+ */
+namespace MyVendorName\MyPackageName;
+
+/**
+ * MyTrait: Example trait.
+ *
+ * Provides some shared behaviour.
+ */
+trait MyTrait
+{
+}
