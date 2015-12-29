16306d1a53f81e041d3ee39414d44eeb84979d95
Eric Mugerwa <ericmdev@gmail.com>
2015-12-29 01:37:36 +0000

    Add src/MyClass.php

---
+++ src/MyClass.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * MyVendorName (http://example.io)
+ *
+ * @package   MyPackageName
+ * @link      https://github.com/user/repo
+ * @license   http://opensource.org/licenses/MIT MIT
+ */
+namespace MyVendorName\MyPackageName;
+
+/**
+ * MyClass: Example class.
+ *
+ * A short description.
+ */
+class MyClass implements MyClassInterface
+{
+}
