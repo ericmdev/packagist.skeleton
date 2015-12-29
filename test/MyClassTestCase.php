e08a62beda3b8c52726aad83ab76419966c9463f
Eric Mugerwa <ericmdev@gmail.com>
2015-12-29 01:14:59 +0000

    Add test/MyClassTestCase.php

---
+++ test/MyClassTestCase.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * MyVendorName (http://example.io)
+ *
+ * @package   MyPackageName
+ * @link      https://github.com/user/repo
+ * @license   http://opensource.org/licenses/MIT MIT
+ */
+namespace MyVendorName\MyPackageNameTest;
+
+/** 
+ * MyClassTestCase
+ *
+ */ 
+abstract class MyClassTestCase
+extends \PHPUnit_Framework_TestCase
+{
+    public function setUp()
+    {
+        // ...
+    }
+    
+    public function tearDown()
+    { 
+        // ...
+    }
+}
